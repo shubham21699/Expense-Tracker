99b0347c4b0e44f89f29973e9d5c308962c85d03
shubham21699 <shubham21699@gmail.com>
2021-12-27 01:26:55 +0530

    All the 3 cards are initialized.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import Details from './components/Details/Details';
+import useStyles from './styles';
+import Main from './components/Main/Main';
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      
+    <div>
+      <Grid className={classes.grid} container spacing={0} alignItems='center' justify='center' style={{ height: '100vh' }}>
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { CallMissedSharp } from '@material-ui/icons';
+import useStyles from './styles';
+
+const Main = () => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardContent>
+                <Typography align='center' variant='h5'>
+                    Total Balance $100
+                </Typography>
+                <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+                    {/* Info card */}
+                    Try saying: Add income for $100 in Category salary for Monday.
+                </Typography>
+                <Divider />
+                {/* Form */}
+            </CardContent>
+            <CardContent className={classes.cartContent}>
+                <Grid container spacing={2} >
+                    <Grid item xs={12} >
+                        {/* <List /> */}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
