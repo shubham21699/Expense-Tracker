a1a01cd0d6391592f2f7cadf0f15505d414c9ac5
shubham21699 <shubham21699@gmail.com>
2021-12-28 19:16:59 +0530

    Local Storage is established for storing all the transactions data and logo of app also changed.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
-import { CallMissedSharp } from '@material-ui/icons';
 import useStyles from './MainStyles';
 import Form from './Form/Form';
 import List from './List/List';
+import { ExpenseTrackerContext } from '../../context/context';
 
 const Main = () => {
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
 
     return (
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
             <CardContent>
                 <Typography align='center' variant='h6'>
-                    Total Balance $100
+                    Total Balance ${balance}
                 </Typography>
                 <Typography variant='subtitle2' style={{ lineHeight: '1em', marginTop: '20px' }}>
                     {/* Info card */}
