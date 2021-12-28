bd2226e8bcd097c021447bfa7a000271233fa66c
shubham21699 <shubham21699@gmail.com>
2021-12-28 11:59:32 +0530

    Voice assistant Speechly integrated into the app.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from './context/context';
+import { SpeechProvider } from '@speechly/react-client';
 
 ReactDOM.render(
-  
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId='3678e0bc-bd15-4e94-976f-3c2af1f97fa1' language='en-US' >
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
   
 );
